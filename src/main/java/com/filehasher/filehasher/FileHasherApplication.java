edb0ae07e1017a8841e3f4333dad4a941ad9968b
cyboranf <cyboranf@gmail.com>
2023-06-16 15:40:12 +0200

    init

---
+++ src/main/java/com/filehasher/filehasher/FileHasherApplication.java
@@ -0,0 +1,13 @@
+package com.filehasher.filehasher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileHasherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileHasherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filehasher/filehasher/FileHasherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filehasher.filehasher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHasherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
